3c59e13c1ea112cee2ad0e07c3bc65b97025df1e
Ruan Lauro <ruanlauromonteiro@gmail.com>
2024-12-26 13:06:18 -0300

    update

--- lib/api.ts
+++ lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://acampamentoadv.netlify.app/api/',  
+  baseURL: 'https://acampamento2025adv.netlify.app/api/',  
   timeout: 5000, 
 });
 
