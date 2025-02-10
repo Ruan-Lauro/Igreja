cd3833c98bfcba96f09e9829c43b6a83195dace2
Ruan Lauro <ruanlauromonteiro@gmail.com>
2025-02-10 14:50:43 -0300

    update

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api/',  
+  baseURL: 'https://acampamento2025adv.netlify.app/api/',  
   timeout: 7000, 
 });
 
 export default axiosInstance;
 
 //https://acampamento2025adv.netlify.app/api/
+//http://localhost:3000/api/
